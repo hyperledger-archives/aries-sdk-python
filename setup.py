db15a064bba9d32db820f56d5716c31e7b2f9426
Daniel Bluhm <daniel.bluhm@sovrin.org>
2019-06-21 16:46:06 -0500

    Add setup file
    Signed-off-by: Daniel Bluhm <daniel.bluhm@sovrin.org>

---
+++ setup.py
@@ -0,0 +1,35 @@
+""" package ariespython """
+
+from setuptools import setup, find_packages
+
+
+def parse_requirements(filename):
+    """Load requirements from a pip requirements file."""
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
+
+if __name__ == '__main__':
+    with open('README.md', 'r') as fh:
+        LONG_DESCRIPTION = fh.read()
+
+    setup(
+        name='aries-sdk-python',
+        version='0.2.0',
+        author='Sam Curren <sam@sovrin.org>, '
+        'Andrew Whitehead <cywolf@gmail.com>, '
+        'Daniel Bluhm <daniel.bluhm@sovrin.org>',
+        description='Idiomatic Python library for Aries-SDK.',
+        long_description=LONG_DESCRIPTION,
+        long_description_content_type='text/markdown',
+        url='https://github.com/hyperledger/aries-sdk-python',
+        license='Apache 2.0',
+        packages=find_packages(),
+        install_requires=parse_requirements('requirements.txt'),
+        python_requires='>=3.6',
+        classifiers=[
+            'Programming Language :: Python :: 3',
+            'License :: OSI Approved :: Apache Software License',
+            'Operating System :: OS Independent'
+        ]
+    )
