4fc58c0d0627e72e9116f9285a2918970197358d
Daniel Bluhm <daniel.bluhm@sovrin.org>
2019-06-24 14:44:21 -0400

    Better errors
    Signed-off-by: Daniel Bluhm <daniel.bluhm@sovrin.org>

--- ariespython/wallet.py
+++ ariespython/wallet.py
@@ -1,37 +1,76 @@
-from indy import wallet
 from typing import Dict, Optional
 import json
+
+from indy import wallet, error
+from .error import errorcode_to_exception
 
 
 async def create_wallet(config: Dict,
                         credentials: Dict) -> None:
-    return await wallet.create_wallet(json.dumps(config), json.dumps(credentials))
+    try:
+        return await wallet.create_wallet(
+            json.dumps(config),
+            json.dumps(credentials)
+        )
+    except error.IndyError as err:
+        raise errorcode_to_exception(err.error_code) from err
 
 
 async def open_wallet(config: Dict,
                       credentials: Dict) -> int:
-    return await wallet.open_wallet(json.dumps(config), json.dumps(credentials))
+    try:
+        return await wallet.open_wallet(
+            json.dumps(config),
+            json.dumps(credentials)
+        )
+    except error.IndyError as err:
+        raise errorcode_to_exception(err.error_code) from err
 
 
 async def close_wallet(handle: int) -> None:
-    return await wallet.close_wallet(handle)
+    try:
+        return await wallet.close_wallet(handle)
+    except error.IndyError as err:
+        raise errorcode_to_exception(err.error_code) from err
 
 
 async def delete_wallet(config: Dict,
                         credentials: Dict) -> None:
-    return await wallet.delete_wallet(json.dumps(config), json.dumps(credentials))
+    try:
+        return await wallet.delete_wallet(
+            json.dumps(config),
+            json.dumps(credentials)
+        )
+    except error.IndyError as err:
+        raise errorcode_to_exception(err.error_code) from err
 
 
 async def export_wallet(handle: int,
                         export_config: Dict) -> None:
-    return await wallet.export_wallet(handle, json.dumps(export_config))
+    try:
+        return await wallet.export_wallet(
+            handle,
+            json.dumps(export_config)
+        )
+    except error.IndyError as err:
+        raise errorcode_to_exception(err.error_code) from err
 
 
 async def import_wallet(config: Dict,
                         credentials: Dict,
                         import_config: Dict) -> None:
-    return await wallet.import_wallet(json.dumps(config), json.dumps(credentials), json.dumps(import_config))
+    try:
+        return await wallet.import_wallet(
+            json.dumps(config),
+            json.dumps(credentials),
+            json.dumps(import_config)
+        )
+    except error.IndyError as err:
+        raise errorcode_to_exception(err.error_code) from err
 
 
 async def generate_wallet_key(config: Optional[Dict]) -> Dict:
-    return json.loads(await wallet.generate_wallet_key(json.dumps(config)))
+    try:
+        return json.loads(await wallet.generate_wallet_key(json.dumps(config)))
+    except error.IndyError as err:
+        raise errorcode_to_exception(err.error_code) from err
