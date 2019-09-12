29b5fc774d297598adc0c8bf5e7d58b0d2ae796c
Andrew Whitehead <cywolf@gmail.com>
2019-09-12 13:47:10 -0700

    use decorator to catch IndyError
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- ariespython/crypto.py
+++ ariespython/crypto.py
@@ -1,49 +1,38 @@
-from typing import Dict, Optional
 import json
+from typing import Optional
 
-from indy import crypto, error
-from .error import errorcode_to_exception
+from indy import crypto
 
-async def pack_message(wallet_handle: int,
-                       message: str,
-                       recipient_verkeys: list,
-                       sender_verkey: Optional[str] = None) -> bytes:
-    try:
-        return await crypto.pack_message(
-            wallet_handle,
-            message,
-            recipient_verkeys,
-            sender_verkey
-        )
-    except error.IndyError as err:
-        raise errorcode_to_exception(err.error_code) from err
+from .error import IndyErrorHandler
 
 
-async def unpack_message(wallet_handle: int,
-                         jwe: bytes) -> (str, str, str):
-    try:
-        unpacked = json.loads(await crypto.unpack_message(wallet_handle, jwe))
-        return (
-            unpacked['message'],
-            unpacked['recipient_verkey'],
-            unpacked.get('sender_verkey', None)
-        )
-    except error.IndyError as err:
-        raise errorcode_to_exception(err.error_code) from err
+@IndyErrorHandler()
+async def pack_message(
+    wallet_handle: int,
+    message: str,
+    recipient_verkeys: list,
+    sender_verkey: Optional[str] = None,
+) -> bytes:
+    return await crypto.pack_message(
+        wallet_handle, message, recipient_verkeys, sender_verkey
+    )
 
 
-async def crypto_sign(wallet_handle: int,
-                      signer_vk: str,
-                      msg: bytes) -> bytes:
-    try:
-        return await crypto.crypto_sign(wallet_handle, signer_vk, msg)
-    except error.IndyError as err:
-        raise errorcode_to_exception(err.errorcode_to_exception) from err
+@IndyErrorHandler()
+async def unpack_message(wallet_handle: int, jwe: bytes) -> (str, str, str):
+    unpacked = json.loads(await crypto.unpack_message(wallet_handle, jwe))
+    return (
+        unpacked["message"],
+        unpacked["recipient_verkey"],
+        unpacked.get("sender_verkey", None),
+    )
 
-async def crypto_verify(signer_vk: str,
-                        msg: bytes,
-                        signature: bytes) -> bool:
-    try:
-        return await crypto.crypto_verify(signer_vk, msg, signature)
-    except error.IndyError as err:
-        raise errorcode_to_exception(err.errorcode_to_exception) from err
+
+@IndyErrorHandler()
+async def crypto_sign(wallet_handle: int, signer_vk: str, msg: bytes) -> bytes:
+    return await crypto.crypto_sign(wallet_handle, signer_vk, msg)
+
+
+@IndyErrorHandler()
+async def crypto_verify(signer_vk: str, msg: bytes, signature: bytes) -> bool:
+    return await crypto.crypto_verify(signer_vk, msg, signature)
